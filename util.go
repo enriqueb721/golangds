854c2b647116666804d3fdf07ea3f5765a147a10
Enrique Burga Gutierrez <eabg97@gmail.com>
2019-07-21 14:28:57 -0500

    Add concurrent functions

--- util.go
+++ util.go
@@ -1,20 +1,30 @@
 package golangds
 
+import "sync"
+
+// storage struct
 type storage struct {
+	sync.RWMutex
 	store []*interface{}
 }
 
 // Empty func
 func (s *storage) Empty() bool {
+	s.RLock()
+	defer s.RUnlock()
 	return len(s.store) == 0
 }
 
 // Size func
 func (s *storage) Size() uint {
+	s.RLock()
+	defer s.RUnlock()
 	return uint(len(s.store))
 }
 
 // Clear func
 func (s *storage) Clear() {
+	s.Lock()
+	defer s.Unlock()
 	s.store = nil
 }
